8b5acf0f19a1b062a90ea9800ddcbc92cf22de49
Sudo9 <joshua.dumka@gmail.com>
2019-05-07 17:26:05 -0400

    Add files via upload

---
+++ A Weird Program That Does Nothing/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim url As String = “https://www.youtube.com/watch?v=oHg5SJYRHA0“
+        Process.Start("https://www.youtube.com/watch?v=oHg5SJYRHA0")
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
